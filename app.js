344c2db5bf2f5321003057a427be7807018541c8
HamnaKhawar <125984152+CodeWithFaiq@users.noreply.github.com>
2023-09-27 22:38:20 +0500

    frst commit

--- app.js
+++ app.js
@@ -51,10 +51,9 @@ app.use(function (err, req, res, next) {
 
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, 
+   
+  )
   .then(() => console.log('Connected to MongoDB...'))
   .catch((error) => console.error('Error connecting to MongoDB:', error.message));
 
