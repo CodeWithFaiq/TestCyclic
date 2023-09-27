ba1bf43f3d277ad8e8725775da5e3829cd82d531
Faiq-sp20-bcs-085 <113433823+Faiq-sp20-bcs-085@users.noreply.github.com>
2023-09-27 20:26:52 +0500

    22t6622222622227

--- app.js
+++ app.js
@@ -47,7 +47,13 @@ app.use(function (err, req, res, next) {
 });
 
 
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error.message));
 
 const conn = mongoose.connection;
 
